850a3ccbe4afd487be1a354b8ffc40ed1584b8d4
Don <don@cranburi.com>
2024-11-16 17:44:35 -0800

    5h 52m adds user story #2 (view account transactions)

--- client/src/components/pages/accounts/ViewAccountPage/index.tsx
+++ client/src/components/pages/accounts/ViewAccountPage/index.tsx
@@ -4,7 +4,11 @@ import { Route as ViewAccountRoute } from "../../../../routes/accounts/$accountI
 import { useListAccountsQuery } from "../../../../lib/api-sdk/hooks/useListAccountsQuery";
 import { buildTimeVariables } from "../../../../buildTimeVariables";
 import { useMemo } from "react";
-import currencyjs from "currency.js";
+import { useListTransactionsQueryQuery } from "../../../../lib/api-sdk/hooks/useListTransactionsQuery";
+import { Account, Transaction } from "../../../../lib/api-sdk/types/entities";
+import { format, isSameYear } from "date-fns";
+import currency from "currency.js";
+import classNames from "classnames";
 
 export const ViewAccountPage = () => {
   const routeParams = ViewAccountRoute.useParams();
@@ -12,8 +16,11 @@ export const ViewAccountPage = () => {
   const listAccountsQuery = useListAccountsQuery({
     host: buildTimeVariables.apiHost,
   });
+  const listTransactionsQuery = useListTransactionsQueryQuery({
+    host: buildTimeVariables.apiHost,
+  });
 
-  const currentAccount = useMemo(() => {
+  const currentAccount: null | Account = useMemo(() => {
     if (listAccountsQuery.isSuccess) {
       const filterResult = listAccountsQuery.data.filter(
         (account) => account.account_id === routeParams.accountId
@@ -23,6 +30,19 @@ export const ViewAccountPage = () => {
 
     return null;
   }, [routeParams, listAccountsQuery.data, listAccountsQuery.isSuccess]);
+
+  const currentTransactions: Transaction[] = useMemo(() => {
+    if (listTransactionsQuery.isSuccess === true) {
+      return listTransactionsQuery.data.filter(
+        (t) => t.account_id === routeParams.accountId
+      );
+    }
+    return [];
+  }, [
+    listTransactionsQuery.isSuccess,
+    listTransactionsQuery.data,
+    routeParams.accountId,
+  ]);
 
   return (
     <div data-test-id="view-account-page" className="bg-white">
@@ -45,15 +65,67 @@ export const ViewAccountPage = () => {
             </span>
           </div>
           <div className="text-center py-4">{currentAccount.name}</div>
-          <div className="text-center py-4">
+          <div className="text-center pt-4 pb-48">
             <div>
               <div className="font-bold text-3xl">
-                {currencyjs(currentAccount.balances.current).format()}
+                {currency(currentAccount.balances.current).format()}
               </div>
               <div className="italic py-2">
-                ({currencyjs(currentAccount.balances.available).format()}{" "}
+                ({currency(currentAccount.balances.available).format()}{" "}
                 available)
               </div>
+            </div>
+          </div>
+          <div>
+            <div className="px-8">
+              <div className="h-16 flex items-center justify-start">
+                <span className="font-bold text-xl">Transactions</span>
+              </div>
+            </div>
+            <div className="px-8">
+              {currentTransactions.map((transaction) => {
+                const transactionDate = new Date(transaction.date);
+                const isCredit = transaction.amount > 0;
+                const isDebit = transaction.amount < 0;
+
+                return (
+                  <div
+                    key={new Date(transaction.date).getDate()}
+                    className="flex py-4"
+                  >
+                    <div className=" flex-shrink flex items-center justify-center p-4">
+                      [icon]
+                    </div>
+                    <div className="flex-grow">
+                      <div>
+                        <span className="font-bold">
+                          {transaction.merchant_name}
+                        </span>
+                      </div>
+                      <div>
+                        {format(transactionDate, "MMM d")}
+                        {isSameYear(transactionDate, new Date()) === true
+                          ? null
+                          : ", " + format(transactionDate, "yyyy")}
+                      </div>
+                    </div>
+                    <div
+                      className={classNames([
+                        "text-xl",
+                        "flex-shrink",
+                        {
+                          "text-green-500": isCredit,
+                          "text-red-500": isDebit,
+                        },
+                      ])}
+                    >
+                      {isCredit && "+"}
+                      {isDebit && "-"}
+                      {currency(transaction.amount).format()}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
